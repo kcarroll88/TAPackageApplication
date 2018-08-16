07a7441665516851f3e31a1617c6dc6ef33da062
Keenan Carroll <keenan.carroll.va@gmail.com>
2018-08-16 18:54:40 -0400

    created a simple package checking system

--- TAPackageApplication/Program.cs
+++ TAPackageApplication/Program.cs
@@ -10,6 +10,31 @@ namespace TAPackageApplication
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            Console.WriteLine("What is the weight of your package?");
+            int weight = Convert.ToInt32(Console.ReadLine());
+            if (weight > 50)
+            {
+                Console.WriteLine("Package is too heavy to be shipped via Package Express. Have a good day.");
+                Console.Read();
+                return;
+            }
+            Console.WriteLine("What is the width of your package?");
+            int width = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("And the height?");
+            int height = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("And what about the length?");
+            int length = Convert.ToInt32(Console.ReadLine());
+            int dimensions = width + height + length;
+            if (dimensions > 50)
+            {
+                Console.WriteLine("Package is too big to be shipped via Package Express.");
+                Console.Read();
+                return;
+            }
+            decimal total = Convert.ToDecimal(dimensions / 100.m);
+            Console.WriteLine("$" + total);
+            Console.Read();
         }
     }
 }
